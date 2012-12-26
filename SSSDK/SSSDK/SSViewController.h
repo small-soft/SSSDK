ae8fb19245e4ed9845f6263fbf879d268d8d6a41
small-soft <small-soft@live.com>
2012-12-26 15:01:33 +0800

    init

---
+++ SSSDK/SSSDK/SSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSViewController.h
+//  SSSDK
+//
+//  Created by 刘 佳 on 12-12-26.
+//  Copyright (c) 2012年 刘 佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSViewController : UIViewController
+
+@end
---
+++ SSSDK/SSSDK/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SSSDK
+//
+//  Created by 刘 佳 on 12-12-26.
+//  Copyright (c) 2012年 刘 佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SSAppDelegate class]));
+    }
+}
